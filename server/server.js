aa2fd1e604d9f461a1bf1fbd58c9bf6f6aeffd51
Aya7m <yyoyo6987@gmail.com>
2025-10-17 05:22:18 +0300

    cors errors

--- server/server.js
+++ server/server.js
@@ -19,35 +19,15 @@ const port = process.env.PORT || 3000
 connection_db()
 cloudinartConfig()
 
-
+app.use(cors())
 app.use(express.json())
 
-const allowedOrigins = [
-  'https://hotal-booking-frontend.vercel.app', // production frontend
-  'http://localhost:5173',                     // local dev
-];
-
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true); // السماح للطلبات بدون origin مثل Postman
-    if(allowedOrigins.indexOf(origin) === -1){
-      return callback(new Error('CORS not allowed for this origin'), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}));
-
-app.options('/api/*', cors());
-
-
-
-// app.use(clerkMiddleware())
+app.use(clerkMiddleware())
 
 app.use('/api/clerk', clerkWebHook)
 
-// ✅ 3. وبعدين clerkMiddleware
-app.use(clerkMiddleware())
+// // ✅ 3. وبعدين clerkMiddleware
+// app.use(clerkMiddleware())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/api/user',userRoute)
